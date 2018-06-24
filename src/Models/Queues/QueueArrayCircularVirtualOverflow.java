b2d1b74ebbc2460bb2355af0425c169626535ef6
George Margaritis <geormarga@gmail.com>
2018-06-24 15:18:09 +0300

    Create base class for all Queue implementations

---
+++ src/Models/Queues/QueueArrayCircularVirtualOverflow.java
@@ -0,0 +1,84 @@
+package Models.Queues;
+
+import Models.Node;
+
+public class QueueArray {
+
+    private Node[] array;
+
+    private int length;
+
+    private Node head, tail;
+
+    public int getLength() {
+        return array.length;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+    /**
+     * Crete a new empty QueueArray by providing the length of the queue.
+     *
+     * @param length
+     */
+    public QueueArray(int length) {
+        array = new Node[length];
+    }
+
+    /**
+     * Create a new QueueArray by providing an array of Node objects.
+     *
+     * @param array
+     */
+    public QueueArray(Node[] array) {
+        this.array = array;
+    }
+
+
+    public Node[] getArray() {
+        return array;
+    }
+
+    public void setArray(Node[] array) {
+        this.array = array;
+    }
+
+    /**
+     * Method that adds another node at the end of the queue.
+     *
+     * @param node The element that is going to be added to the end of the queue.
+     */
+    public void enque(Node node) {
+        // Catch the first element that gets inserted.
+        if (head == null) {
+            array[0] = node;
+            head = node;
+            tail = node;
+        } else {
+            int index = java.util.Arrays.asList(array).indexOf(tail);
+            array[++index] = node;
+            tail = node;
+        }
+    }
+
+    /**
+     * Method that removes the first node from the start of the queue.
+     */
+    public Node deque() {
+        int headIndex = java.util.Arrays.asList(array).indexOf(head);
+        int tailIndex = java.util.Arrays.asList(array).indexOf(tail);
+        if (array[headIndex] == array[tailIndex]) {
+
+        } else {
+            array[headIndex] = null;
+            head = array[++headIndex];
+        }
+
+        return array[0];
+    }
+}
