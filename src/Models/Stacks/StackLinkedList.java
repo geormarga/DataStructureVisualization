f5a6eb8cd9c01dd44e0f539a20d18765e23dfa12
George Margaritis <geormarga@gmail.com>
2018-07-15 18:56:50 +0300

    Implement linked list for Stack in StackLinkedList.java

--- src/Models/Stacks/StackLinkedList.java
+++ src/Models/Stacks/StackLinkedList.java
@@ -1,4 +1,23 @@
 package Models.Stacks;
 
-public class StackLinkedList {
+import Models.Node;
+
+public class StackLinkedList implements Stack<Node>{
+
+    Node bottom, top;
+
+    @Override
+    public void push(Node node) {
+        top.setNext(node);
+    }
+
+    @Override
+    public Node pop() {
+        Node returnValue = top;
+        if (top == null){
+            //underflow exception
+        }
+        top = null;
+        return returnValue;
+    }
 }
