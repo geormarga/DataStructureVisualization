34ae3357b04dc662afc3cc9db4b69ced379050d8
George Margaritis <geormarga@gmail.com>
2018-07-15 18:33:45 +0300

    Add setNext method, constructor and data members in Node.java

--- src/Models/Node.java
+++ src/Models/Node.java
@@ -1,3 +1,15 @@
 package Models;
-public class Node extends Object{
+
+public class Node extends Object {
+
+    private String data;
+    private Node next;
+
+    public Node(String data) {
+        this.data = data;
+    }
+
+    public void setNext(Node node) {
+        this next = node;
+    }
 }
