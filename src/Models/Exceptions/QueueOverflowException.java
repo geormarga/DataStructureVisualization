a092a818a38bf1ed9a796e7eb26328fb091325d1
George Margaritis <geormarga@gmail.com>
2018-07-29 22:01:37 +0300

    Override exception message in QueueOverflowException.java

--- src/Models/Exceptions/QueueOverflowException.java
+++ src/Models/Exceptions/QueueOverflowException.java
@@ -1,4 +1,7 @@
 package Models.Exceptions;
 
 public class QueueOverflowException extends Exception{
+    QueueOverflowException(){
+        super("Queue has reached its maximum capacity.");
+    }
 }
