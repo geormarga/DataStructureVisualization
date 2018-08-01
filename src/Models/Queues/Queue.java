fbefff05e859d6c55eede2df7a5ae8fcfc8a780b
George Margaritis <geormarga@gmail.com>
2018-08-01 23:00:51 +0300

    Add throws exception to interface methods of Queue.java

--- src/Models/Queues/Queue.java
+++ src/Models/Queues/Queue.java
@@ -1,5 +1,8 @@
 package Models.Queues;
 
+import Models.Exceptions.QueueOverflowException;
+import Models.Exceptions.QueueUnderflowException;
+
 public interface Queue<T>{
 
     /**
@@ -7,12 +10,12 @@ public interface Queue<T>{
      *
      * @param t The element that is added to the end of the queue.
      */
-    void enqueue(T t);
+    void enqueue(T t) throws QueueOverflowException;
 
     /**
      * Method that removes the first element from the start of the queue.
      *
      * @return The element that is removed from the start of the queue.
      */
-    T dequeue();
+    T dequeue() throws QueueUnderflowException;
 }
