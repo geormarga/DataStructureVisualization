b00aab8c1fd216fc69b46561b520823653bcba1d
George Margaritis <geormarga@gmail.com>
2018-08-01 23:13:36 +0300

    Create new empty test classes for Models/Queues

---
+++ test/Models/Queues/QueueArrayShiftingVirtualOverflowTest.java
@@ -0,0 +1,54 @@
+package Models.Queues;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueueArrayShiftingVirtualOverflowTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void handle() {
+    }
+
+    @Test
+    void getLength() {
+    }
+
+    @Test
+    void getHead() {
+    }
+
+    @Test
+    void getTail() {
+    }
+
+    @Test
+    void getArray() {
+    }
+
+    @Test
+    void setArray() {
+    }
+
+    @Test
+    void containsAny() {
+    }
+
+    @Test
+    void enqueue() {
+    }
+
+    @Test
+    void dequeue() {
+    }
+}
---
+++ test/Models/Queues/QueueArrayTest.java
@@ -0,0 +1,50 @@
+package Models.Queues;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueueArrayTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getLength() {
+    }
+
+    @Test
+    void getHead() {
+    }
+
+    @Test
+    void getTail() {
+    }
+
+    @Test
+    void getArray() {
+    }
+
+    @Test
+    void setArray() {
+    }
+
+    @Test
+    void containsAny() {
+    }
+
+    @Test
+    void enqueue() {
+    }
+
+    @Test
+    void dequeue() {
+    }
+}
