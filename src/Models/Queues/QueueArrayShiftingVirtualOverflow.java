fc6e148cfd193bdab0e7bc0af78a29440ddb6ef2
George Margaritis <geormarga@gmail.com>
2018-08-11 14:52:29 +0300

    Fix handle and enqueue method in QueueArrayShiftingVirtualOverflow.java

--- src/Models/Queues/QueueArrayShiftingVirtualOverflow.java
+++ src/Models/Queues/QueueArrayShiftingVirtualOverflow.java
@@ -1,5 +1,7 @@
 package Models.Queues;
 
+import Models.Exceptions.QueueOverflowException;
+import Models.Exceptions.VirtualOverflowException;
 import Models.Interfaces.IVirtualOverflow;
 import Models.Node;
 
@@ -26,21 +28,41 @@ public class QueueArrayShiftingVirtualOverflow extends QueueArray implements Que
 
 
     @Override
+    public void enqueue(Node node) throws QueueOverflowException {
+        // The first block will only executed the first time around.
+        if (array[head] == null) {
+            // Adds the node to the array.
+            // Sets the head and tail to the last inserted node.
+            array[head] = node;
+            array[tail] = node;
+            //if any of the array elements are null and the tail is at the last element
+        } else if (!containsAny(array, null)) {
+            throw new QueueOverflowException();
+        } else if (array[tail] == array[size - 1]) {
+            handle(array);
+            array[++tail] = node;
+        } else {
+            array[++tail] = node;
+        }
+    }
+
+    /**
+     * Method that shifts the elements until the head is in the first element of the list.
+     * It puts the next element's value to the current position and sets the next element's value to null.
+     *
+     * @param array
+     */
+    @Override
     public void handle(Node[] array) {
-        // Determine how much the difference between the head and the tail is. (tail - head)
-        // Determine how much the difference between the first element and the head is. head
-        // Shift all the elements to the first place and fill the others with null.
-        // The head is going to be set to the front of the queue and the tail to the front + the difference the head from the tail has.
-        //int diff = head - tail;
         for (int i = 0; i < array.length; i++) {
-            if (i <= (tail - head) && ((head + i) < array.length)) {
-                array[i] = array[head + i];
-                array[head + i] = null;
-            } else {
-                array[i] = null;
+            // If it is the last element this swap should not happen, as it is empty due to the shift.
+            if ((i - 1) == array.length) {
+                array[i] = array[i + 1];
+                array[i + 1] = null;
             }
         }
-        tail = array.length - head;
+        // The new tail will be the difference between the last tail and the last head.
+        tail = tail - head;
         head = 0;
     }
 }
