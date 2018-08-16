088e3ec1ef3f9479a29a32c5fc41ec2df1f8514e
George Margaritis <geormarga@gmail.com>
2018-08-16 19:14:10 +0300

    Add getter for tail and head and fix enqueue and dequeue methods in QueueLinkedList.java

--- src/Models/Queues/QueueLinkedList.java
+++ src/Models/Queues/QueueLinkedList.java
@@ -1,26 +1,40 @@
 package Models.Queues;
 
+import Models.Exceptions.QueueUnderflowException;
 import Models.Node;
 
 public class QueueLinkedList implements Queue<Node> {
 
-    Node head, tail;
+
+    private Node head, tail;
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
 
     @Override
     public void enqueue(Node node) {
         if (head == null) {
             head = node;
+        } else {
+            tail.setNext(node);
         }
-        tail.setNext(node);
+        tail = node;
     }
 
     @Override
     public Node dequeue() {
-        Node returnValue = head;
-        if (head == null){
-            //underflow exception
+        if (head == null) {
+            throw new QueueUnderflowException();
         }
         head = head.getNext();
-        return returnValue;
+        if(head == null){
+            tail = null;
+        }
+        return head;
     }
 }
