60d133d44617e9ca26a4bbf5e2d278faade76ef3
George Margaritis <geormarga@gmail.com>
2018-08-18 15:17:55 +0300

    Add routing in StackController.java

--- src/sample/Contollers/StackController.java
+++ src/sample/Contollers/StackController.java
@@ -1,4 +1,34 @@
 package sample.Contollers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class StackController {
+
+    @FXML
+    public Button arrayButton, linkedListButton;
+    @FXML
+    VBox parentVbox;
+
+    public void initialize() {
+        arrayButton.setOnAction(e -> switchScene("../resources/Stack/StackArrayImplementation.fxml"));
+        linkedListButton.setOnAction(e -> switchScene("../resources/Stack/StackLinkedListImplementation.fxml"));
+    }
+
+    public void switchScene(String resource) {
+        Stage stage = (Stage) parentVbox.getScene().getWindow();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(resource));
+            stage.setScene(new Scene(root));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
