82ce372496660539dcbab9c97e81b0e7de91637e
George Margaritis <geormarga@gmail.com>
2018-09-15 13:13:01 +0300

    Remove dead code and fix identation.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -8,32 +9,24 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
-    private static Stage primaryStageObj;
-
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStageObj = primaryStage;
 
         try {
             Parent root = FXMLLoader.load(getClass().getResource("sample/resources/Home.fxml"));
             primaryStage.initStyle(StageStyle.UNDECORATED);
             //primaryStage.setFullScreen(true);
             primaryStage.setTitle("Data Structure Visualization");
-            primaryStage.setScene(new Scene(root,800,800));
+            primaryStage.setScene(new Scene(root, 800, 800));
+
             primaryStage.show();
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-
-    }
-
-
-    public Stage getPrimaryStageObj() {
-        return primaryStageObj;
     }
 }
