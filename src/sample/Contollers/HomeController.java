c7824d7aa1082eda50990c98aa19801f1f3f34bc
George Margaritis <geormarga@gmail.com>
2018-09-22 18:48:18 +0300

    Remove both data and function members that aren't used anymore.

--- src/sample/Contollers/HomeController.java
+++ src/sample/Contollers/HomeController.java
@@ -2,21 +2,16 @@ package sample.Contollers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
 
 public class HomeController extends BaseViewController {
 
     @FXML
-    public Button queueButton, stackButton;
-    @FXML
-    private Pane pane;
+    private Button queueButton, stackButton;
 
     @Override
     public void initialize() {
         super.initialize();
-        pane.toBack();
         queueButton.setOnAction(e -> switchScene("../resources/QueueSelector.fxml"));
         stackButton.setOnAction(e -> switchScene("../resources/StackSelector.fxml"));
-        btnClose.setOnAction(e -> System.exit(0));
     }
 }
--- src/sample/Contollers/QueueController.java
+++ src/sample/Contollers/QueueController.java
@@ -2,21 +2,15 @@ package sample.Contollers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
 
 public class QueueController extends BaseViewController {
 
-
     @FXML
-    public Button arrayButton, linkedListButton, arrayShiftingButton, arrayCircularButton, backButton;
-    @FXML
-    private Pane pane;
-
+    private Button arrayButton, linkedListButton, arrayShiftingButton, arrayCircularButton, backButton;
 
     @Override
     public void initialize() {
         super.initialize();
-        pane.toBack();
         arrayButton.setOnAction(e -> switchScene("../resources/Queue/QueueArrayImplementation.fxml"));
         linkedListButton.setOnAction(e -> switchScene("../resources/Queue/QueueLinkedListImplementation.fxml"));
         arrayShiftingButton.setOnAction(e -> switchScene("../resources/Queue/QueueArrayShiftingVirtualOverflowImplementation.fxml"));
