16caa3aca857daf998fc012b5183b048b3948242
George Margaritis <geormarga@gmail.com>
2018-10-25 21:30:05 +0300

    Add displayallAsList method to IVisible

--- src/Models/Interfaces/IVisible.java
+++ src/Models/Interfaces/IVisible.java
@@ -1,4 +1,8 @@
 package Models.Interfaces;
 
-public interface IVisible {
+import java.util.List;
+
+public interface IVisible<T> {
+    List<T> displayAllAsList();
+//    Node should be displayed in a certain way,then some controllers should do the work of handling the changes in each data structure (each list)
 }
---
+++ src/sample/Contollers/NodeElement.java
@@ -0,0 +1,4 @@
+package sample.Contollers;
+
+public class NodeElement {
+}
