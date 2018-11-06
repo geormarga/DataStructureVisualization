8398d4693aad2291067eb3e2571c680cacc783ac
George Margaritis <geormarga@gmail.com>
2018-11-06 23:13:56 +0200

    Add logic to hide tracker label and arrow and logic to bind width of child to parent.

--- src/sample/Contollers/NodeElement.java
+++ src/sample/Contollers/NodeElement.java
@@ -1,15 +1,9 @@
 package sample.Contollers;
 
-import javafx.beans.NamedArg;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import sample.resources.Arrow;
-
-import java.io.IOException;
 
 
 public class NodeElement extends VBox {
@@ -23,7 +17,7 @@ public class NodeElement extends VBox {
         tracker = new Label("top bottom");
         node = new Label("default");
         arrow = new Pane();
-        arrow.getChildren().add(new Arrow(this.getLayoutX(),this.getLayoutY()));
+        arrow.getChildren().add(new Arrow(this.getLayoutX(), this.getLayoutY()));
         index = new Label("0");
         //get parent's initial x and y
         tracker.getStyleClass().add("tracker-text");
@@ -37,8 +31,7 @@ public class NodeElement extends VBox {
         this.getChildren().add(index);
     }
 
-    public NodeElement(@NamedArg("isFirst") boolean isFirst, @NamedArg("isLast") boolean isLast, @NamedArg("nodeText") String nodeText, @NamedArg("indexText") String indexText) {
-
+    public NodeElement(boolean isFirst, boolean isLast, String nodeText, String indexText) {
         super();
         String trackerText;
         if (isFirst && isLast) {
@@ -53,18 +46,26 @@ public class NodeElement extends VBox {
         tracker = new Label(trackerText);
         node = new Label(nodeText);
         arrow = new Pane();
-        arrow.getChildren().add(new Arrow(this.getLayoutX(),this.getLayoutY()));
+        arrow.getChildren().add(new Arrow(this.getLayoutX(), this.getLayoutY()));
         index = new Label(indexText);
-        //get parent's initial x and y
-        //arrow.getChildren().add();
+
         tracker.getStyleClass().add("tracker-text");
         arrow.getStyleClass().add("arrow");
         node.getStyleClass().add("node");
         index.getStyleClass().add("index-text");
 
+        if (!isFirst && !isLast) {
+            arrow.setVisible(false);
+        }
+
         this.getChildren().add(tracker);
         this.getChildren().add(arrow);
         this.getChildren().add(node);
         this.getChildren().add(index);
+
+        tracker.prefWidthProperty().bind(this.widthProperty());
+        arrow.prefWidthProperty().bind(this.widthProperty());
+        node.prefWidthProperty().bind(this.widthProperty());
+        index.prefWidthProperty().bind(this.widthProperty());
     }
 }
