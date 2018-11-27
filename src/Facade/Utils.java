d074547a552596c4d0bd2244c41d26b213b453e3
George Margaritis <geormarga@gmail.com>
2018-11-27 14:59:56 +0200

    Update localisation files

--- src/Facade/Utils.java
+++ src/Facade/Utils.java
@@ -1,4 +1,27 @@
 package Facade;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Utils {
+    private static final ObjectProperty<Locale> locale = new SimpleObjectProperty<>(Locale.getDefault());
+
+    public static ObjectProperty<Locale> localeProperty() {
+        return locale;
+    }
+
+    public static Locale getLocale() {
+        return locale.get();
+    }
+
+    public static void setLocale(Locale locale) {
+        localeProperty().set(locale);
+    }
+
+    public static String i18n(String key) {
+        return ResourceBundle.getBundle("Facade.resources.application", getLocale()).getString(key);
+    }
 }
