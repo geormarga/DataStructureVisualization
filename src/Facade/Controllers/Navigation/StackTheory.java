c9f4985c51ab5819d8cc4efa842872972a2e961d
George Margaritis <geormarga@gmail.com>
2018-11-29 13:16:15 +0200

    Add temporary class field for text in StackTheory.java

--- src/Facade/Controllers/Navigation/StackTheory.java
+++ src/Facade/Controllers/Navigation/StackTheory.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 
@@ -13,6 +14,8 @@ public class StackTheory implements ISelection {
 
     @FXML
     private Button backButton;
+    @FXML
+    private Text texttext;
 
     @Override
     public void initialize() {
