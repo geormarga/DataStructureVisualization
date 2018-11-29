6cbe034491ddced68b63b15a903357629c71ee54
George Margaritis <geormarga@gmail.com>
2018-11-29 21:32:21 +0200

    Implement logic and view for handling QueueLinkedListImplementation

--- src/Facade/Controllers/QueueLinkedListController.java
+++ src/Facade/Controllers/QueueLinkedListController.java
@@ -3,7 +3,7 @@ package Facade.Controllers;
 import Facade.CustomElements.QueueNodeElement;
 import Facade.Interfaces.ISelection;
 import Models.Node;
-import Models.Queues.QueueArray;
+import Models.Queues.QueueLinkedList;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -12,26 +12,20 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.TilePane;
-import javafx.scene.layout.VBox;
-
-import java.util.List;
 
 public class QueueLinkedListController implements ISelection {
 
     @FXML
+    private ObservableList<QueueNodeElement> visibleList = FXCollections.observableArrayList();
+    @FXML
     TilePane tilePane;
     @FXML
-    Button enqueueButton, dequeueButton;
+    Button enqueueButton, dequeueButton, clearButton;
     @FXML
     TextField nodeInputTextfield;
     @FXML
     Label nodeErrorLabel;
-    @FXML
-    VBox actionGroup;
-    @FXML
-    private ObservableList<QueueNodeElement> visibleList = FXCollections.observableArrayList();
-
-    private QueueArray queueArray;
+    private QueueLinkedList queueLinkedList;
 
     @Override
     public void switchScene(String resource) {
@@ -42,72 +36,55 @@ public class QueueLinkedListController implements ISelection {
     public void initialize() {
         enqueueButton.setOnAction(e -> clickOnEnqueueButton());
         dequeueButton.setOnAction(e -> clickOnDequeueButton());
-        nodeInputTextfield.textProperty().addListener((observable, oldValue, newValue) -> nodeInputTextfield.setText(checkForTextfieldLimit(oldValue, newValue, 9)));
+        clearButton.setOnAction(e -> clickOnClearButton());
     }
 
     private void clickOnEnqueueButton() {
         try {
-            clearValidationMessages();
-            String text = nodeInputTextfield.getText();
-            boolean textIsEmpty = text.equals("");
-            if (!textIsEmpty) {
-                queueArray.enqueue(new Node(text));
-                updateNodeElements();
-            } else {
-                setValidationText(nodeErrorLabel);
-            }
-            nodeInputTextfield.clear();
+        if (queueLinkedList == null) {
+            queueLinkedList = new QueueLinkedList();
+            visibleList = FXCollections.observableArrayList();
+            tilePane.getChildren().addAll(visibleList);
+        }
+        clearValidationMessages();
+        String text = nodeInputTextfield.getText();
+        boolean textIsEmpty = text.equals("");
+        if (!textIsEmpty) {
+            queueLinkedList.enqueue(new Node(text));
+            visibleList.add(new QueueNodeElement(text, ""));
+            tilePane.getChildren().clear();
+            tilePane.getChildren().addAll(visibleList);
+        } else {
+            setValidationText(nodeErrorLabel);
+        }
+        nodeInputTextfield.clear();
         } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            System.out.println(ex.getStackTrace());
         }
     }
 
     private void clickOnDequeueButton() {
         try {
             clearValidationMessages();
-            queueArray.dequeue();
-            updateNodeElements();
+            queueLinkedList.dequeue();
+            visibleList.remove(visibleList.get(0));
+            tilePane.getChildren().clear();
+            tilePane.getChildren().addAll(visibleList);
         } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            System.out.println(ex.getStackTrace());
         }
     }
 
     /**
-     * Method that creates a new queue array and based on that returns a node-element list.
-     *
-     * @param size The queue's size
-     * @return The Nodelements created in correspondence with queue array's  status.
+     * Method that creates a new stack with the exact same size the previous one had.
+     * <p>
+     * Which is actually remove values from nodes
+     * If there was no previous node there is nothing to clear.
      */
-    private ObservableList<QueueNodeElement> createNodes(int size) {
-        queueArray = new QueueArray(size);
-        ObservableList<QueueNodeElement> returnList = FXCollections.observableArrayList();
-        for (int i = 0; i < size; i++) {
-            returnList.add(new QueueNodeElement("", Integer.toString(i)));
-        }
-        return returnList;
-    }
-
-    /**
-     * Method that updates the list of node-elements displayed according to the latest status of the queue Array.
-     */
-    private void updateNodeElements() {
-        List<Node> nodeList = queueArray.displayAllAsList();
-        int size = nodeList.size();
-        QueueNodeElement displayNode;
-        Node node;
-
-        for (int i = 0; i < size; i++) {
-            displayNode = visibleList.get(i);
-            displayNode.setNodeData("");
-            visibleList.get(i).setTracker(false, false);
-        }
-
-        for (int i = 0; i < size; i++) {
-            displayNode = visibleList.get(i);
-            node = nodeList.get(i);
-            displayNode.setNodeData(node == null ? "" : node.getData());
-            visibleList.get(i).setTracker(i == queueArray.getTail(), i == queueArray.getHead());
-        }
+    private void clickOnClearButton() {
+        clearValidationMessages();
+        queueLinkedList = null;
+        visibleList.removeAll();
         tilePane.getChildren().clear();
         tilePane.getChildren().addAll(visibleList);
     }
@@ -128,9 +105,4 @@ public class QueueLinkedListController implements ISelection {
         parent.getStyleClass().clear();
         parent.getStyleClass().add("label-no-error");
     }
-
-    private String checkForTextfieldLimit(String oldValue, String newValue, int limit) {
-        return (newValue.length()) <= limit ? newValue : oldValue;
-    }
-
 }
