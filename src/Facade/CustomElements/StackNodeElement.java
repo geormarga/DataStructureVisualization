3c616cc129bf6c6e1f6a97b1206ee6f3e6ed5bb1
George Margaritis <geormarga@gmail.com>
2018-11-30 16:44:24 +0200

    Fix layout issues with element's auto-size and remove dead code.

--- src/Facade/CustomElements/StackNodeElement.java
+++ src/Facade/CustomElements/StackNodeElement.java
@@ -3,27 +3,22 @@ package Facade.CustomElements;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 
 public class StackNodeElement extends HBox {
 
 
-    private Label tracker, node, index;
-    private Pane arrow;
-    private VBox arrowGroup;
+    private Label tracker = new Label("top"), node, index;
+    private Pane arrow = new Pane();
 
-    public StackNodeElement() {
+    public StackNodeElement(String nodeText, String indexText) {
         super();
-        tracker = new Label("top bottom");
-        node = new Label("default");
-        arrow = new Pane();
-        arrowGroup = new VBox();
-        arrow.getChildren().add(new HorizontalArrow(this.getLayoutX(), this.getLayoutY()));
-        index = new Label("0");
+        node = new Label(nodeText);
+        index = new Label(indexText);
         //get parent's initial x and y
+        arrow.getChildren().add(new HorizontalArrow(this.getLayoutX(), this.getLayoutY()));
+
         tracker.getStyleClass().add("tracker-text");
         arrow.getStyleClass().add("h-arrow");
-        arrowGroup.getChildren().addAll(tracker, arrow);
         node.getStyleClass().add("node");
         index.getStyleClass().add("index-text");
 
@@ -31,50 +26,24 @@ public class StackNodeElement extends HBox {
         this.getChildren().add(arrow);
         this.getChildren().add(node);
         this.getChildren().add(index);
+
+        this.tracker.setVisible(false);
+        this.arrow.setVisible(false);
+
+        tracker.prefHeightProperty().bind(this.heightProperty());
+        arrow.prefHeightProperty().bind(this.heightProperty());
+        node.prefHeightProperty().bind(this.heightProperty());
+        index.prefHeightProperty().bind(this.heightProperty());
     }
 
-    public StackNodeElement(String nodeText, String indexText) {
-        super();
-        tracker = new Label("");
-        node = new Label(nodeText);
-        arrow = new Pane();
-        //get parent's initial x and y
-        arrow.getChildren().add(new HorizontalArrow(this.getLayoutX(), this.getLayoutY()));
-        arrowGroup = new VBox();
-        index = new Label(indexText);
-
-
-        tracker.getStyleClass().add("tracker-text");
-        arrow.getStyleClass().add("h-arrow");
-        node.getStyleClass().add("node");
-        index.getStyleClass().add("index-text");
-
-        arrowGroup.getChildren().addAll(tracker, arrow);
-
-        this.getChildren().add(arrowGroup);
-        arrowGroup.setVisible(false);
-        this.getChildren().add(node);
-        this.getChildren().add(index);
-
-        arrowGroup.prefWidthProperty().bind(this.heightProperty());
-        node.prefWidthProperty().bind(this.heightProperty());
-        index.prefWidthProperty().bind(this.heightProperty());
-    }
-
-    public void setTracker(boolean isFirst, boolean isLast) {
+    public void setTracker(boolean isFirst) {
         this.tracker.getText();
-        if (isFirst && isLast) {
-            this.tracker.setText("top bottom");
-            this.arrowGroup.setVisible(true);
-        } else if (isFirst) {
-            this.tracker.setText("top");
-            this.arrowGroup.setVisible(true);
-        } else if (isLast) {
-            this.tracker.setText("bottom");
-            this.arrowGroup.setVisible(true);
+        if (isFirst) {
+            this.tracker.setVisible(true);
+            this.arrow.setVisible(true);
         } else {
-            this.tracker.setText("");
-            this.arrowGroup.setVisible(false);
+            this.tracker.setVisible(false);
+            this.arrow.setVisible(false);
         }
     }
 
