0f98bd9a6ffa09ef661bd43035501de45bbe32a5
George Margaritis <geormarga@gmail.com>
2019-02-07 12:07:40 +0200

    Change text label from "tail head"->"head tail"

--- src/Facade/CustomElements/QueueNodeElement.java
+++ src/Facade/CustomElements/QueueNodeElement.java
@@ -11,25 +11,6 @@ public class QueueNodeElement extends VBox {
     private Label tracker, node, index;
     private Pane arrow;
 
-    public QueueNodeElement() {
-        super();
-        tracker = new Label("top bottom");
-        node = new Label("default");
-        arrow = new Pane();
-        arrow.getChildren().add(new VerticalArrow(this.getLayoutX(), this.getLayoutY()));
-        index = new Label("0");
-        //get parent's initial x and y
-        tracker.getStyleClass().add("tracker-text");
-        arrow.getStyleClass().add("v-arrow");
-        node.getStyleClass().add("node");
-        index.getStyleClass().add("index-text");
-
-        this.getChildren().add(tracker);
-        this.getChildren().add(arrow);
-        this.getChildren().add(node);
-        this.getChildren().add(index);
-    }
-
     public QueueNodeElement(String nodeText, String indexText) {
         super();
         tracker = new Label("");
@@ -58,7 +39,7 @@ public class QueueNodeElement extends VBox {
     public void setTracker(boolean isFirst, boolean isLast) {
         this.tracker.getText();
         if (isFirst && isLast) {
-            this.tracker.setText("tail head");
+            this.tracker.setText("head tail");
             this.arrow.setVisible(true);
         } else if (isFirst) {
             this.tracker.setText("tail");
